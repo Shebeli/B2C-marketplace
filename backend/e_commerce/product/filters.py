7c590b1a7c84ec34e7c7c2eb890734e1d6b11cab
shervin <shervinsaeedi78@gmail.com>
2024-07-31 21:37:42 +0330

    feat(product): update 'ProductFilter'

--- backend/e_commerce/product/filters.py
+++ backend/e_commerce/product/filters.py
@@ -3,24 +3,24 @@ from product.models import Product
 
 
 class ProductFilter(filters.FilterSet):
-    name = filters.CharFilter(field_name="name", lookup_expr="icontain")
+    """
+    It is assumed that the 'Product' querysets that are used within this filter,
+    and their 'is_valid' fields are true (the 'main_variant' and 'owner'
+    fields are not null on the Product instance).
+    """
+
+    name = filters.CharFilter(field_name="name", lookup_expr="icontains")
     subcategory = filters.CharFilter(
         field_name="subcategory__name", lookup_expr="iexact"
     )
-    tags = filters.CharFilter(method="filter_by_tag_names")
-    price_min = filters.NumberFilter(field_name="main_price", lookup_expr="gte")
-    price_max = filters.NumberFilter(field_name="main_price", lookup_expr="lte")
-    in_stock = filters.BooleanFilter(method="filter_in_stock")
+    price_min = filters.NumberFilter(
+        field_name="main_variant__price", lookup_expr="gte"
+    )
+    price_max = filters.NumberFilter(
+        field_name="main_variant__price", lookup_expr="lte"
+    )
+    in_stock = filters.BooleanFilter()
 
     class Meta:
         model = Product
-        fields = ["in_stock", "subcategory", "price_min", "price_max", "name"]
-
-    def filter_in_stock(self, queryset, name, value):
-        if value:
-            return queryset.available()
-        return queryset
-
-    def filter_by_tag_names(self, queryset, name, value):
-        tag_names = value.split(",")
-        return queryset.filter(tags__name__in=tag_names).distinct()
+        fields = ["name", "subcategory", "tags"]
