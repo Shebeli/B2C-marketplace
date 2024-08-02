ca0ff44ee05be20fb1dd3f5fa3e05f448903c199
shervin <shervinsaeedi78@gmail.com>
2024-08-02 16:15:37 +0330

    refactor(product): main variant assignment signal improvements
    - Add atomic transactions for associated signals
    - Change deletion signal type from post_delete to pre_delete
    - Enhance 'main_variant' assignment logic in deletion signal due to signal type change
    - Optimize product.save() method using 'update_field'

--- backend/e_commerce/product/signals.py
+++ backend/e_commerce/product/signals.py
@@ -1,20 +1,30 @@
-from django.db.models.signals import post_save, post_delete
+from django.db import transaction
+from django.db.models.signals import post_save, pre_delete
 from django.dispatch import receiver
 from .models import ProductVariant
 
 
 @receiver(post_save, sender=ProductVariant)
 def assign_main_variant_on_creation(sender, instance, created, **kwargs):
-    product = instance.product
-    if not product.main_variant:
-        instance.product.main_variant = instance
-        instance.product.save()
+    if created:
+        with transaction.atomic():
+            product = instance.product
+            if not product.main_variant:
+                instance.product.main_variant = instance
+                instance.product.save(update_fields=["main_variant"])
 
 
-@receiver(post_delete, sender=ProductVariant)
+@receiver(pre_delete, sender=ProductVariant)
 def assign_main_variant_on_deletion(sender, instance, **kwargs):
     product = instance.product
-    if product.main_variant == instance:
-        new_main_variant = ProductVariant.objects.filter(product=product).first()
-        product.main_variant = new_main_variant  # might be null
-        product.save()
+    with transaction.atomic():
+        if product.main_variant == instance:
+            new_main_variant = (
+                ProductVariant.objects.filter(product=product)
+                .exclude(id=instance.id)
+                .first()
+            )
+            product.main_variant = (
+                new_main_variant  # might be null if no other variants exist
+            )
+            product.save(update_fields=["main_variant"])
