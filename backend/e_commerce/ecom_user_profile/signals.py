5843aa9a1cba641adb35d35fbc5eaf9146bb8228
Shervin <Shervinsaeedi78@gmail.com>
2024-11-01 19:54:41 +0330

    fix(backend): moved all object creations signals for new user creations into a single module

--- backend/e_commerce/ecom_user_profile/signals.py
+++ backend/e_commerce/ecom_user_profile/signals.py
@@ -1,6 +1,8 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from ecom_user.models import EcomUser
+from financeops.models import Wallet
+from order.models import Cart
 
 from .models import CustomerProfile, SellerProfile
 
@@ -10,3 +12,5 @@ def create_profiles(sender, instance, created, **kwargs):
     if created:
         SellerProfile.objects.create(user=instance)
         CustomerProfile.objects.create(user=instance)
+        Wallet.objects.create(user=instance)
+        Cart.objects.create(user=instance)
