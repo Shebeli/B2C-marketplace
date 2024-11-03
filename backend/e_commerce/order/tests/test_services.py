473d1f3e46129040b5b3ff26abcd79dd99ba1d77
Shervin <Shervinsaeedi78@gmail.com>
2024-11-03 16:10:51 +0330

    test(backend): add new test cases for order management services

--- backend/e_commerce/order/tests/test_services.py
+++ backend/e_commerce/order/tests/test_services.py
@@ -1,5 +1,10 @@
 import pytest
-from order.services.management import process_order_creation, pay_order_using_wallet
+from order.services.management import (
+    process_order_creation,
+    pay_order_using_wallet,
+    update_order_to_shipped,
+    update_order_to_cancelled,
+)
 from order.models import Cart, CartItem, Order
 from financeops.models import Transaction
 
@@ -9,10 +14,8 @@ from financeops.models import Transaction
 
 
 @pytest.mark.django_db
-def test_process_order_creation(
-    customer_and_seller, sample_product_instance_factory
-):
-    customer, seller = customer_and_seller()
+def test_process_order_creation(customer_and_seller, sample_product_instance_factory):
+    customer, seller = customer_and_seller
     product = sample_product_instance_factory()
 
     cart_total_price = 0
@@ -45,9 +48,7 @@ def test_process_order_creation(
 
 
 @pytest.mark.django_db
-def test_pay_order_using_wallet(
-    customer_and_seller, sample_product_instance_factory
-):
+def test_pay_order_using_wallet(customer_and_seller, sample_product_instance_factory):
     customer, seller = customer_and_seller
     product = sample_product_instance_factory()
     for variant in product.variants.all():
@@ -74,3 +75,100 @@ def test_pay_order_using_wallet(
     assert transaction_obj.type == Transaction.WALLET_PAYMENT
     assert transaction_obj.amount == order.get_total_price()
     assert transaction_obj.order == order
+
+
+@pytest.mark.django_db
+def test_updating_order_to_shipped(
+    customer_and_seller, sample_product_instance_factory
+):
+    # prepare the data
+    customer, seller = customer_and_seller
+    product = sample_product_instance_factory()
+    for variant in product.variants.all():
+        CartItem.objects.create(
+            cart=customer.cart,
+            product_variant=variant,
+            quantity=10,
+        )
+    data = {
+        "user": customer,
+        "notes": "ooooo",
+        "customer_address": customer.customer_addresses.first(),
+    }
+    order = process_order_creation(data)
+    pay_order_using_wallet(customer.wallet, order)
+    order.refresh_from_db()
+
+    # updating order to SHIPPED process
+    shipping_data = {
+        "tracking_code": 12341234,
+    }
+    first_variant, second_variant = product.variants.first(), product.variants.last()
+    pre_update_reserved_stocks = (
+        first_variant.reserved_stock,
+        second_variant.reserved_stock,
+    )
+    pre_update_on_hand_stocks = (
+        first_variant.on_hand_stock,
+        second_variant.on_hand_stock,
+    )
+    update_order_to_shipped(order, shipping_data)
+    first_variant.refresh_from_db()
+    second_variant.refresh_from_db()
+
+    assert order.status == Order.SHIPPED
+    assert order.tracking_code == shipping_data["tracking_code"]
+    assert pre_update_reserved_stocks == (
+        first_variant.reserved_stock + 10,
+        second_variant.reserved_stock + 10,
+    )
+    assert pre_update_on_hand_stocks == (
+        first_variant.on_hand_stock + 10,
+        second_variant.on_hand_stock + 10,
+    )
+
+
+@pytest.mark.django_db
+def test_cancelling_unpaid_order(customer_and_seller, sample_product_instance_factory):
+    # prepare the data
+    customer, seller = customer_and_seller
+    product = sample_product_instance_factory()
+    for variant in product.variants.all():
+        CartItem.objects.create(
+            cart=customer.cart,
+            product_variant=variant,
+            quantity=10,
+        )
+    order_creation_data = {
+        "user": customer,
+        "notes": "ooooo",
+        "customer_address": customer.customer_addresses.first(),
+    }
+    order = process_order_creation(order_creation_data)
+    order_cancel_data = {"cancel_reason": "too many orders"}
+    first_variant, second_variant = product.variants.first(), product.variants.last()
+    pre_update_reserved_stocks = (
+        first_variant.reserved_stock,
+        second_variant.reserved_stock,
+    )
+    initial_wallet_balance = customer.wallet.balance
+    pay_order_using_wallet(customer.wallet, order)
+    refund_transaction = update_order_to_cancelled(order, order_cancel_data)
+    first_variant.refresh_from_db()
+    second_variant.refresh_from_db()
+
+    assert order.status == Order.CANCELLED
+    assert order.cancel_reason
+    assert pre_update_reserved_stocks == (
+        first_variant.reserved_stock + 10,
+        second_variant.reserved_stock + 10,
+    )
+
+    # since the payment was completed using wallet, its
+    # expected that the paid amount has been refunded to
+    # customer's wallet.
+
+    customer.wallet.refresh_from_db()
+    assert customer.wallet.balance == initial_wallet_balance
+    assert refund_transaction.type == Transaction.WALLET_REFUND
+    assert refund_transaction.amount == order.get_total_price()
