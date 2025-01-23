1510e0ead4f481a81b672b4597df7443ff8f0dc3
Shervin <Shervinsaeedi78@gmail.com>
2025-01-24 00:06:03 +0330

    feat(frontend): early implemenation of phone verification page (no backend integration)

--- frontend/b2c-app/src/components/Authentication/AuthLayout.tsx
+++ frontend/b2c-app/src/components/Authentication/AuthLayout.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-// Abstract component used as a parent component by other auth components like login component.
+// Abstract component.
 const AuthLayout: React.FC = () => {
   return (
     <div className="flex items-center justify-center h-screen">
--- frontend/b2c-app/src/components/Authentication/VerifyPhone.tsx
+++ frontend/b2c-app/src/components/Authentication/VerifyPhone.tsx
@@ -1,10 +1,41 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { MainLogo } from "../../assets/MainLogo";
 import { phoneNumberValidator } from "@persian-tools/persian-tools";
 
 const VerifyPhone: React.FC = () => {
   const [submittedPhone, setSubmittedPhone] = useState<string | null>(null);
   const [isSessionValid, setSessionIsValid] = useState<boolean>(true);
+  const [verificationCode, setVerificationCode] = useState<string[]>(
+    Array(5).fill(null)
+  );
+  const [isFormInvalid, setIsFormInvalid] = useState<boolean>(false);
+  const inputsRef = useRef<HTMLInputElement[]>([]);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
+    if (verificationCode.some((element) => element === null)) {
+      e.preventDefault();
+      setIsFormInvalid(true);
+      return;
+    }
+    e.preventDefault();
+    setIsFormInvalid(false);
+    console.log(
+      `Phone verification code form submitted: ${verificationCode.join("")}`
+    );
+  };
+
+  const handleInput =
+    (index: number) => (e: React.ChangeEvent<HTMLInputElement>) => {
+      const newValue = e.target.value;
+      if (newValue.length == 1) {
+        setVerificationCode((prev) =>
+          prev.map((item, i) => (i === index ? newValue : item))
+        );
+        if (index < inputsRef.current.length - 1) {
+          inputsRef.current[index + 1].focus();
+        }
+      }
+    };
 
   useEffect(() => {
     const phone = localStorage.getItem("inputPhone");
@@ -15,19 +46,32 @@ const VerifyPhone: React.FC = () => {
     }
     if (phone) {
       // validate OTP's age.
-      setSubmittedPhone(submittedPhone);
+      setSubmittedPhone(phone);
       const now = Date.now();
-      const tenMinutes = 10 * 60 * 1000; // in milliseconds
-      const submittedTimeStamp = new Date(phoneTimeStamp);
+      const submittedTimeStamp = new Date(phoneTimeStamp).getTime();
+      const codeExpiriation = 15 * 60 * 1000; // in milliseconds
 
-      if (now - submittedTimeStamp.getTime() >= tenMinutes) {
+      // is code expired
+      if (now - submittedTimeStamp >= codeExpiriation) {
         setSessionIsValid(false);
+        return;
       }
-      if (!phoneNumberValidator(submittedPhone)) {
+      // is phone valid
+      if (!phoneNumberValidator(phone)) {
         setSessionIsValid(false);
+        return;
       }
     }
   }, []);
+
+  useEffect(() => {
+    if (verificationCode.every((element) => element !== null)) {
+      const constructedCode = verificationCode.join("");
+      console.log(
+        `The code is constructed: ${constructedCode}, should be sent to server for verification.`
+      );
+    }
+  }, [verificationCode]);
 
   return (
     <div className="p-6 rounded-xl border-2 flex flex-col  gap-3 shadow-sm">
@@ -36,21 +80,69 @@ const VerifyPhone: React.FC = () => {
       </a>
 
       {isSessionValid ? (
-        <div>
-          <p> لطفا کد تایید ارسال به شماره را وارد نمایید: {submittedPhone}</p>
-          <button className="btn btn-primary self-center h-11 btn-sm mt-4 w-full  text-lg font-bold">
-            تایید
-          </button>
+        <div className="flex flex-col gap-2 items-center text-lg">
+          <p className="mb-2">
+            {" "}
+            لطفا کد تایید ارسال شده به شماره {submittedPhone} را وارد نمایید:
+          </p>
+          {isFormInvalid && (
+            <p className="text-error font-medium">
+              لطفا کد را بصورت کامل وارد نمایید
+            </p>
+          )}
+          <form
+            className="flex flex-col items-center"
+            onSubmit={handleSubmit}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                e.preventDefault();
+                handleSubmit(e as unknown as React.FormEvent<HTMLFormElement>);
+              }
+            }}
+          >
+            <div className="flex gap-1.5 flex-row-reverse">
+              {Array.from({ length: 5 }, (_, index) => (
+                <input
+                  key={index}
+                  type="text"
+                  maxLength={1}
+                  ref={(element) => (inputsRef.current[index] = element!)}
+                  onChange={handleInput(index)}
+                  className={`input input-bordered w-12 text-2xl ${isFormInvalid ? "input-error" : ""}`}
+                  inputMode="numeric"
+                  pattern="[0-9]*"
+                  onKeyDown={(e) => {
+                    if (e.key === "Tab" || e.key === "Shift") {
+                      return;
+                    }
+                    if (!/^\d$/.test(e.key)) {
+                      e.preventDefault();
+                    }
+                  }}
+                ></input>
+              ))}
+            </div>
+            <button
+              className="btn btn-primary h-11 btn-sm mt-4 w-full  text-lg font-bold "
+              type="submit"
+            >
+              تایید
+            </button>
+          </form>
         </div>
       ) : (
-        <p className="text-sm text-error font-bold">
-          زمان هویت سنجی تلفن وارد شده منقضی شده است. لطفا مجددا شماره تلفن خود
-          را در
-          <a className="link" href="/login">
-            این لینک
-          </a>
-          وارد نمایید
-        </p>
+        <div className="text-lg flex flex-col items-center">
+          <p> زمان هویت سنجی تلفن منقضی شده است. </p>
+          <p>
+            {" "}
+            می توانید{" "}
+            <a className="link link-info" href="/login">
+              {" "}
+              در این لینک
+            </a>{" "}
+            اقدام به ورود مجدد نمایید.
+          </p>
+        </div>
       )}
     </div>
   );
