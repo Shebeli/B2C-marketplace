3de77688cf2706e88210a2c09e51d2bb45ca52ba
Shervin <Shervinsaeedi78@gmail.com>
2025-01-25 15:01:15 +0330

    fix(backend): fix celery settings not working due to a typo

--- backend/e_commerce/ecom_core/celery.py
+++ backend/e_commerce/ecom_core/celery.py
@@ -1,7 +1,7 @@
 import os 
 from celery import Celery
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULES", "ecom_core.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ecom_core.settings")
 app = Celery("ecom_core")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
