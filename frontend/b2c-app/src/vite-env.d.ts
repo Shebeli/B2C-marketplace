215d8731c1596652728b3f998293330e20690b0a
Shervin <Shervinsaeedi78@gmail.com>
2025-02-08 16:43:41 +0330

    fix(frontend): add navbar profile followup config

--- frontend/b2c-app/src/vite-env.d.ts
+++ frontend/b2c-app/src/vite-env.d.ts
@@ -1,7 +1,8 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-    readonly VITE_API_BASE_URL: string 
+    readonly VITE_API_BASE_URL: string
+    readonly VITE_API_TOKEN_REFRESH: string 
     readonly VITE_CODE_LIFESPAN: number
     readonly VITE_CODE_REQUEST_COOLDOWN: number
 }
