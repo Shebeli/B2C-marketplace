dcc098040adae9f58f75ae1fdbd2549e30fbdd78
Shervin <Shervinsaeedi78@gmail.com>
2025-02-08 16:32:52 +0330

    feat(backend): add new navbar_info endpoint for UserAccountViewSet

--- backend/e_commerce/ecom_user/views.py
+++ backend/e_commerce/ecom_user/views.py
@@ -1,23 +1,20 @@
 from django.core.cache import cache
 from rest_framework import status
 from rest_framework.decorators import action
-from rest_framework.generics import get_object_or_404
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
 from rest_framework.viewsets import ViewSet
 from rest_framework_simplejwt.tokens import RefreshToken
-import time
+
 
 from ecom_user.authentication import EcomUserJWTAuthentication
 from ecom_user.models import EcomUser
-from ecom_user.permissions import IsAnonymous
 from ecom_user.serializers import (
     ChangeCurrentPasswordSerializer,
-    OTPAuthSerializer,
-    OTPAuthVerificationSerializer,
     UserAccountSerializer,
     UserPhoneSerializer,
     UserPhoneVerificationSerializer,
+    UserNavbarSerializer,
 )
 from ecom_user.sms_service import (
     create_phone_verify_cache_key,
@@ -37,7 +34,7 @@ class UserLoginViewSet(ViewSet):
       an EcomUser instance will be created with an unusable password using the phone number.
     """
 
-    permission_classes = [IsAnonymous]
+    permission_classes = [AllowAny]
 
     @action(detail=False, methods=["post"], throttle_classes=[SMSAnonRateThrottle])
     def request_code(self, request):
@@ -88,6 +85,9 @@ class UserLoginViewSet(ViewSet):
         except EcomUser.DoesNotExist:
             user = serializer.save()
         refresh = RefreshToken.for_user(user)
+        refresh["user_type"] = (
+            "normal"  # payload for distinguishing normal users from admin users
+        )
         return Response(
             {
                 "refresh": str(refresh),
@@ -97,72 +97,12 @@ class UserLoginViewSet(ViewSet):
         )
 
 
-# class UserOnetimeAuthViewSet(ViewSet):
-#     """
-#     [DEPRECATED: use UserSignupViewSet actions instead.]
-#     Should be used when the user forgets their password or they want to login without
-#     inputting their password, which gives them access to one time authentication using OTP via SMS.
-#     Provides the following actions:
-#     - request_auth: sends an OTP via SMS to the inputted phone
-#       number, which is required in the next action.
-#     - verify_auth_request: verify the one time authentication using the OTP recieved
-#       from previous action.
-#     """
-
-#     permission_classes = [IsAnonymous]
-
-#     @action(detail=False, methods=["post"], throttle_classes=[SMSAnonRateThrottle])
-#     def request_auth(self, request):
-#         serializer = OTPAuthSerializer(data=request.data)
-#         serializer.is_valid(raise_exception=True)
-#         user = get_object_or_404(EcomUser, phone=serializer.data["phone"])
-#         code_cooldown_time = cache.ttl(create_sms_cooldown_cache_key(user.phone))
-#         if code_cooldown_time:
-#             return Response(
-#                 {"cooldown time to request another code": f"{code_cooldown_time}s"},
-#                 status=status.HTTP_429_TOO_MANY_REQUESTS,
-#             )
-#         process_phone_verification(serializer.data["phone"])
-#         return Response(
-#             {
-#                 "success": f"verification code has been sent via SMS to this phone number: {user.phone}"
-#             },
-#             status=status.HTTP_202_ACCEPTED,
-#         )
-
-#     @action(
-#         detail=False, methods=["post"], throttle_classes=[CodeSubmitAnonRateThrottle]
-#     )
-#     def verify_auth_request(self, request):
-#         serializer = OTPAuthVerificationSerializer(data=request.data)
-#         serializer.is_valid(raise_exception=True)
-#         cached_code = cache.get(create_phone_verify_cache_key(serializer.data["phone"]))
-#         if not cached_code:
-#             return Response(
-#                 {"error": "server is not expecting a verification code for this phone"},
-#                 status=status.HTTP_400_BAD_REQUEST,
-#             )
-#         if serializer.data["verification_code"] != cached_code:
-#             return Response(
-#                 {"error": "verification code is incorrect"},
-#                 status=status.HTTP_400_BAD_REQUEST,
-#             )
-#         user = EcomUser.objects.get(phone=serializer.data["phone"])
-#         refresh = RefreshToken.for_user(user)
-#         return Response(
-#             {
-#                 "refresh": str(refresh),
-#                 "access": str(refresh.access_token),
-#             },
-#             status=status.HTTP_200_OK,
-#         )
-
-
 class UserAccountViewSet(ViewSet):
     """
     Provides the following actions (requires the current user to be authenticated):
-    - get_info: retrieves the current user account info.
+    - account_info: retrieves the current authenticated user's account info.
     - update_info: update the current user account info (except phone, email and password)
+    - navbar_info: retrieves the current authenticated user's navbar info
     - change_password: for changing current password.
     - change_phone_request: for changing user's current phone password by requesting an OTP via SMS,
       a verification code is sent to the new phone and then used in the next action
@@ -173,6 +113,11 @@ class UserAccountViewSet(ViewSet):
 
     authentication_classes = [EcomUserJWTAuthentication]
     permission_classes = [IsAuthenticated]
+
+    @action(detail=False, methods=["get"])
+    def navbar_info(self, request):
+        serializer = UserNavbarSerializer(request.user)
+        return Response(serializer.data)
 
     @action(detail=False, methods=["get"])
     def get_info(self, request):
